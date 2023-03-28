4a8323bc222b6f1e4c52fbf70894ce2d27b9b52d
ManoliShah03 <manoli.shah@sigmainfo.net>
2023-03-28 09:23:17 +0530

    wordpress assignment2

---
+++ wordpress/wp-content/themes/generatepress-child/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+if ( !defined( 'ABSPATH' ) ) exit;
+
+if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
+    function chld_thm_cfg_locale_css( $uri ){
+        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
+            $uri = get_template_directory_uri() . '/rtl.css';
+        return $uri;
+    }
+endif;
+add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
+
